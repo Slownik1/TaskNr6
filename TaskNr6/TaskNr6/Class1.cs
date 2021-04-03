2b5d0053e196cf21e6d1910039aea1ee2ee4891a
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-04-03 10:39:04 +0200

    Add files via upload

---
+++ TaskNr6/TaskNr6/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskNr6
+{
+    public struct Class1
+    {
+
+        public int x;
+        public int y;
+        public static char symbol = 'X';
+
+        public enum Direction: int
+        {
+
+            right=1,
+            left = -1,
+            up = 1,
+            down =-1,
+
+        }
+
+        public static void Move(int x, int y)
+        {
+
+            int width = (Console.WindowWidth)-1;
+            int height = Console.WindowHeight;
+
+            Console.SetCursorPosition(x, y);
+
+            while (true)
+            {
+                var control = Console.ReadKey();
+
+                switch (control.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.Clear();
+                        y--;
+                        if ((x < 0) || (x > width) || (y > height) || (y < 0))
+                            y += 2;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(symbol);
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        Console.Clear();
+                        y++;
+                        if ((x < 0) || (x > width) || (y > height) || (y < 0))
+                            y -= 2;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(symbol);
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        Console.Clear();
+                        x--;
+                        if ((x < 0) || (x > width) || (y > height) || (y < 0))
+                            x += 2;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(symbol);
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        Console.Clear();
+                        x++;
+                        if ((x < 0) || (x > width) || (y > height) || (y < 0))
+                            x-=2;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(symbol);
+                        break;
+
+                    default:
+                        Environment.Exit(0);
+                        break;
+
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ TaskNr6/TaskNr6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskNr6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Move(3, 5);
+
+            
+        }
+    }
+}
